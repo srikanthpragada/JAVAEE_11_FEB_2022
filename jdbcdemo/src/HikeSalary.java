9b6c31b858c667bf3ed63c172d7d2bf7e3de87e3
srikanthpragada <srikanthpragada@yahoo.com>
2022-02-14 09:01:26 +0530

    14-FEB

---
+++ jdbcdemo/src/HikeSalary.java
@@ -0,0 +1,19 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class HikeSalary {
+
+	public static void main(String[] args) throws Exception {
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/hr", "root", "mysql");
+		Statement st = con.createStatement();
+
+		int count = st.executeUpdate("update employees set salary = salary + 50000 where id = 5");
+		System.out.printf("Updated %d row(s)", count);
+		st.close();
+		con.close();
+	}
+
+}
